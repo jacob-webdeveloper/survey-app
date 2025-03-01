b6cede9f061b016b93ce380dee95502302e53578
Jacob Kochergen <jacobkochergen@Jacobs-MacBook-Air.local>
2025-03-01 23:04:09 +1030

    mongodb connection added

--- server/server.js
+++ server/server.js
@@ -1,10 +1,13 @@
 require("dotenv").config()
 
 const express = require("express")
+const mongoose = require("mongoose")
 const treeRoutes = require("./routes/trees")
 
+// express app
 const app = express()
 
+// middleware
 app.use(express.json())
 
 app.use((req, res, next) => {
@@ -12,8 +15,19 @@ app.use((req, res, next) => {
     next()
 })
 
+// routes
 app.use("/api/trees", treeRoutes)
 
-app.listen(process.env.PORT, () => {
-    console.log("listening on port 4000")
-})
+// Connect to db
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        // listen for requests
+    app.listen(process.env.PORT, () => {
+        console.log("connected to db & listening on port", process.env.PORT)
+    })
+    })  
+    .catch((error) => {
+        console.log(error)
+    })
+
+
